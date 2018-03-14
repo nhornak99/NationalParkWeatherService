f3c7d3f1274377ac42be7b2c93fcef837108f2f6
Noah <nhornak99@gmail.com>
2018-03-14 09:39:17 -0400

    Init commit

---
+++ NPGeek.Web/Models/DALS/ParkDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NPGeek.Web.Models.DALS
+{
+    public class ParkDAL
+    {
+
+        public List<Park> GetAllParks()
+        {
+            List<Park> parks = new List<Park>();
+
+            return parks;
+        }
+    }
+}
