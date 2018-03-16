3d310a795f076d665749af15e1904b81a1c6981d
Noah <nhornak99@gmail.com>
2018-03-16 10:18:30 -0400

    Fixed bug for switching temp from fahr to celc

--- NPGeek.Web/Content/WeatherConverter.js
+++ NPGeek.Web/Content/WeatherConverter.js
@@ -1,8 +1,12 @@
 ﻿function WeatherConversion() {
-    
-    var temp = document.cookie;    
 
-    if (temp == "fahrenheit" || temp == null) {
+    var temp = document.cookie;
+
+    if (temp == null || temp == "") {
+        ToCelcius();
+        document.cookie = "celcius";
+    }
+    else if (temp == "fahrenheit") {
         ToCelcius();
         document.cookie = "celcius";
     }
@@ -14,20 +18,27 @@
 
 
 
-function ToFahrenheit() {
-
-    var weather = document.getElementsByClassName('weather-conversion');
-
-    for (var i = 0; i < weather.length; i++) {
-        weather[i].textContent = Math.round((weather[i].textContent - 32) / 1.8);
-    }
-}
-
 function ToCelcius() {
 
     var weather = document.getElementsByClassName('weather-conversion');
 
     for (var i = 0; i < weather.length; i++) {
+        weather[i].textContent = Math.round((weather[i].textContent - 32) * (5 / 9));
+    }
+}
+
+function ToFahrenheit() {
+
+    var weather = document.getElementsByClassName('weather-conversion');
+
+    for (var i = 0; i < weather.length; i++) {
         weather[i].textContent = Math.round((weather[i].textContent * 1.8) + 32);
+    }
+}
+document.onload = Load();
+
+function Load(){
+    if (document.cookie == "celcius") {
+        ToCelcius();
     }
 }
